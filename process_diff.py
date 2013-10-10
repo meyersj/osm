fdcf6ff5e07c5cab0a298b4f85a345ba0c849137
meyersj <meyers2@pdx.edu>
2013-10-09 17:26:36 -0700

    working on multi to single part multipolygon

--- process_diff.py
+++ process_diff.py
@@ -1,17 +1,18 @@
-from shapely.geometry import mapping, shape
+from shapely.geometry import mapping, shape, Polygon
 from shapely.ops import cascaded_union
 from fiona import collection
-
+import ast
 
 rlis = 'P:/osm/rlis2osm_verify/7_county/scripts/test_data/rlis_trails.shp'
 osm = 'P:/osm/rlis2osm_verify/7_county/scripts/test_data/osm_trails.shp'
 osm_buffer = 'P:/osm/rlis2osm_verify/7_county/scripts/test_data/osm_buffer.shp'
 osm_buffer_union = 'P:/osm/rlis2osm_verify/7_county/scripts/test_data/osm_buffer_union.shp'
 diff =  'P:/osm/rlis2osm_verify/7_county/scripts/test_data/diff.shp'
-
+osm_buffer_union_split = 'P:/osm/rlis2osm_verify/7_county/scripts/test_data/osm_buffer_union_split.shp'
 
 schema = { 'geometry': 'Polygon' , 'properties': {'name': 'str'} }
 
+"""
 with collection(osm, 'r') as input:
   for line in input:
     print shape(line['geometry'])
@@ -53,7 +54,60 @@ with collection(osm_buffer, 'r') as input:
       },
       'geometry': mapping(merged)
     })
+"""
 
+
+polygons = []
+
+count = 1
+with collection(osm_buffer_union, 'r') as input:
+  for polygon in input:
+    string = str(shape(polygon['geometry']))
+
+    #print string
+    coord = string.split('(', 1)[1].split(',')
+    for item in coord:
+      print item
+    #print coord
+ 
+    """
+    #f_polygon = {}
+    for f in coord:
+      end = False
+      f = f.strip()
+      if f[0] == '{' or f[0] == '('i:
+        f = f[1:]
+      elif f[-1] == '}':
+        f = f[0:-1]
+        end = True
+ 
+      f = f.split(' ')
+      x = float(f[0])
+      y = float(f[1])
+      pair = x , y
+      if end == False:
+        f_polygon['pair'] =
+      else:
+        f_polygon.append(pair)
+        polygons.append(f_polygon)
+        f_polygon = []
+    """
+    #print polygons
+    #polygons = ast.literal_eval(polygons)
+  
+    #for polygon in polygons:
+      #print polygon
+     
+  with collection(osm_buffer_union_split, 'w', 'ESRI Shapefile', schema) as output:
+    for poly in polygons:
+      f = Polygon(poly)
+      #print shape(f)
+      #output.write({
+	#'properties': {
+	#  'name': 'test'
+	# },
+        #'geometry': mapping(shape(f))
+      #})
 """
 with collection(rlis, 'r') as input_rlis:
   with collection(osm_buffer_union) as input_osm
